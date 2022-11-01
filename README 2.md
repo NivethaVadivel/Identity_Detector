dab266fae507f3fd7f4232b6526ae12f12069a26
raguram <p.raguram121@gmail.com>
2022-11-01 11:29:53 +0530

    Mail and SMS Added

--- README 2.md
+++
@@ -1,1 +0,0 @@
-# Identity_Detector
---
+++ keys.py
@@ -0,0 +1,5 @@
+account_sid ='AC24c895bd06a08cedb3a876086d14e73f'
+auth_token ='a9c8d0458d50f11c1f4cef3c36f0254b'
+
+twilio_number='+12059736899'
+target_number ='+917010317400'
